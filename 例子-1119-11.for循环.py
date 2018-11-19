bfa035ecac002d3acf75491697996b66ca6a2a2f
blackplusy <happyming07@sina.com>
2018-11-19 17:23:45 +0800

    Add files via upload

---
+++ 例子-1119-11.for循环.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+str1='python is no.1'
+for i in str1:
+	print(i)
+#补充：空格也是字符串
+
+li=['5kong','8jie','shasir']
+for i in li:
+	print(i)
+
+#函数range()
+#range(10)	从0开始，0-9
+#range(1,10)	从1开始，1-9
+
+for i in range(10):
+	print(i)
+
+for i in range(1,10):
+	print(i)
+
+
+
+
+
+
+
