a240273ffe9ce04d8e41d8d3feedd7b43285f146
blackplusy <happyming07@sina.com>
2018-11-19 10:09:41 +0800

    Add files via upload

---
+++ 例子-1119-01.输出.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+#name=raw_input('请输入您的大号：')
+#print(name)
+
+name=input('your name is ?')
+print(name)
---
+++ 例子-1119-02.输入.py
@@ -0,0 +1,46 @@
+#coding=utf-8
+#直接输出是通过print()函数进行打印
+#打印翻滚吧牛宝宝
+print('翻滚吧！牛宝宝！')
+
+#变量输出
+#name='python'
+#print(name)
+#name=input('请输入您的年龄：')
+#print(name)
+
+#变量操作后输出
+a=20
+b=30
+print(a+b)
+a='python'
+b='no.1'
+print(a+b)
+
+#函数输出
+#abs(绝对值)
+print(abs(-10))
+
+#格式化输出
+# %s	输出字符串
+# %d	输出整型
+name=input('请输入您的大号：')
+age=int(input('请输入您的年龄：'))
+print('您的大号是%s,您的年龄是%d'%(name,age))
+
+#补充：数据类型转换   int() 转换为整型 str（）转换为字符串
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
