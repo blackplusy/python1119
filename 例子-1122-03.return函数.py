7822fef2ddd45d4a1196b9128088fdd6e130245a
blackplusy <happyming07@sina.com>
2018-11-22 10:47:42 +0800

    Add files via upload

---
+++ 例子-1122-03.return函数.py
@@ -0,0 +1,16 @@
+def sum(a,b):
+	jisuan=a+b
+	return jisuan
+s=sum(20,30)
+print(s)
+
+print('**************')
+def re(a,b):
+	a*=10
+	b*=10
+	return a,b
+#num=re(4,5)
+#print(num)
+#print(type(num))
+num1,num2=re(10,50)
+print(num1,num2)
